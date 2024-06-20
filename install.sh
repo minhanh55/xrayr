b715dd6bd971e330a1a4f638aaf21cc2c0cb66b6
Huỳnh Minh Anh <122679072+minhanh55@users.noreply.github.com>
2024-06-20 07:47:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 clear
 echo "   1. Cài đặt"
-echo "   2. update config"
+echo "   2. update"
 echo "   3. thêm node"
-read -p "  Vui lòng chọn một số và nhấn Enter (Enter theo mặc định Cài đặt):  " num
+read -p "  Vui lòng chọn một số và nhấn Enter (Enter theo mặc định Cài đặt)  " num
 [ -z "${num}" ] && num="1"
 
 
-
-install_1(){
+install(){
   clear
   # Gán giá trị mặc định cho api_host và api_key
   api_host="minhanhvpn.com"
@@ -20,57 +19,53 @@ install_1(){
   echo "--------------------------------"
   echo "Bạn đã chọn key web: ${api_key}"
   echo "--------------------------------"
-  #IP vps
-#  read -p "Nhập ip vps :" CertDomain
-#   [ -z "${CertDomain}" ] && CertDomain="0"
-#  echo "-------------------------------"
-#   echo "ip vps : ${CertDomain}"
-#  echo "-------------------------------"
-
-
 
   pre_install
+  
 }
+	
 pre_install(){
  clear
-		read -p "Nhập số node cần cài và nhấn Enter (tối đa 2 node): " n
+	read -p "Nhập số node cần cài và nhấn Enter (tối đa 2 node): " n
 	 [ -z "${n}" ] && n="1"
+    a=0
     if [ "$n" -ge 2 ] ; then 
     n="2"
-    fi
-    a=0
+fi
   while [ $a -lt $n ]
-do
-echo -e "node thứ $((a+1))"
+ do
+ echo " node số $((a+1))"
 
 
- echo -e "[1] Vmess"
- echo -e "[2] Vless"
+  echo -e "[1] Vmess"
+  echo -e "[2] Vless"
   echo -e "[3] trojan"
   echo -e "[4] Shadowsocks"
-  read -p "chọn kiểu node(mặc định là v2ray):" NodeType
+  read -p "chọn kiểu node(mặc định là vmess):" NodeType
   if [ "$NodeType" == "1" ]; then
     NodeType="V2ray"
-    NodeName="Vmess"
     EnableVless="false"
+    info="vmess"
   elif [ "$NodeType" == "2" ]; then
     NodeType="V2ray"
-    NodeName="Vless"
     EnableVless="true"
+    info="Vless"
   elif [ "$NodeType" == "3" ]; then
     NodeType="Trojan"
-    NodeName="Trojan"
     EnableVless="false"
-  elif [ "$NodeType" == "4" ]; then
+    info="Trojan"
+    elif [ "$NodeType" == "4" ]; then
     NodeType="Shadowsocks"
-    NodeName="Shadowsocks"
     EnableVless="false"
+    info="Shadowsocks"
   else
     NodeType="V2ray"
     EnableVless="false"
+    info="vmess"
   fi
-  echo "Bạn đã chọn $NodeName"
+  echo "Bạn đã chọn $info"
   echo "--------------------------------"
+
 
 
   #node id
@@ -79,17 +74,13 @@ echo -e "node thứ $((a+1))"
   echo "-------------------------------"
   echo -e "Node_ID: ${node_id}"
   echo "-------------------------------"
-
-
-
-  
-
   
 
  config
   a=$((a+1))
 done
 }
+
 
 
 #clone node
@@ -106,32 +97,38 @@ clone_node(){
   echo "--------------------------------"
   echo "Bạn đã chọn key web: ${api_key}"
   echo "--------------------------------"
-
-#node type
   
- echo -e "[1] Vmess"
- echo -e "[2] Vless"
+  echo -e "[1] Vmess"
+  echo -e "[2] Vless"
   echo -e "[3] trojan"
   echo -e "[4] Shadowsocks"
-  read -p "chọn kiểu node(mặc định là v2ray):" NodeType
+  read -p "chọn kiểu node(mặc định là vmess):" NodeType
   if [ "$NodeType" == "1" ]; then
     NodeType="V2ray"
     EnableVless="false"
+    info="vmess"
   elif [ "$NodeType" == "2" ]; then
     NodeType="V2ray"
     EnableVless="true"
+    info="Vless"
   elif [ "$NodeType" == "3" ]; then
     NodeType="Trojan"
     EnableVless="false"
-  elif [ "$NodeType" == "4" ]; then
+    info="Trojan"
+    elif [ "$NodeType" == "4" ]; then
     NodeType="Shadowsocks"
     EnableVless="false"
+    info="Shadowsocks"
   else
     NodeType="V2ray"
     EnableVless="false"
+    info="vmess"
   fi
-  echo "Bạn đã chọn $NodeType"
+  echo "Bạn đã chọn $info"
   echo "--------------------------------"
+
+
+
 
 
   #node id
@@ -140,21 +137,11 @@ clone_node(){
   echo "-------------------------------"
   echo -e "Node_ID: ${node_id}"
   echo "-------------------------------"
-  
-  
-
-  
-#   #IP vps
-#  read -p "Nhập domain :" CertDomain
-#   [ -z "${CertDomain}" ] && CertDomain="0"
-#  echo "-------------------------------"
-#   echo "ip : ${CertDomain}"
-#  echo "-------------------------------"
-
+ 
 
  config
-  
- 
+#   a=$((a+1))
+#   done
 }
 
 
@@ -166,40 +153,66 @@ clone_node(){
 config(){
 cd /etc/XrayR
 cat >>config.yml<<EOF
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, , Proxypanel
+  - PanelType: "V2board" # Panel type:  NewV2board, V2board
     ApiConfig:
       ApiHost: "https://$api_host"
       ApiKey: "$api_key"
       NodeID: $node_id
       NodeType: $NodeType # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
-      EnableVless: $EnableVless # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      EnableVless: $EnableVless  # Enable Vless for V2ray Type
+      VlessFlow: "none" # Only support vless
       SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
       DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
       RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
+      DisableCustomConfig: false # disable custom config for sspanel
     ControllerConfig:
+      DisableSniffing: true
       ListenIP: 0.0.0.0 # IP address you want to listen
       SendIP: 0.0.0.0 # IP address you want to send pacakage
       UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
       EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
       DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableSniffing: True # Disable domain sniffing
       EnableProxyProtocol: false # Only works for WebSocket and TCP
+      AutoSpeedLimitConfig:
+        Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)
+        WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.
+        LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)
+        LimitDuration: 0 # How many minutes will the limiting last (unit: minute)
+      GlobalDeviceLimitConfig:
+        Enable: false # Enable the global device limit of a user
+        RedisAddr: 127.0.0.1:6379 # The redis server address
+        RedisPassword: YOUR PASSWORD # Redis password
+        RedisDB: 0 # Redis DB
+        Timeout: 5 # Timeout for redis request
+        Expiry: 60 # Expiry time (second)
       EnableFallback: false # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
+        - SNI: # TLS SNI(Server Name Indication), Empty for any
           Alpn: # Alpn, Empty for any
           Path: # HTTP PATH, Empty for any
           Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable
+      DisableLocalREALITYConfig: false  # disable local reality config
+      EnableREALITY: false # Enable REALITY
+      REALITYConfigs:
+        Show: false # Show REALITY debug
+        Dest: www.smzdm.com:443 # Required, Same as fallback
+        ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for disable
+        ServerNames: # Required, list of available serverNames for the client, * wildcard is not supported at the moment.
+          - www.smzdm.com
+        PrivateKey: YOUR_PRIVATE_KEY # Required, execute './xray x25519' to generate.
+        MinClientVer: # Optional, minimum version of Xray client, format is x.y.z.
+        MaxClientVer: # Optional, maximum version of Xray client, format is x.y.z.
+        MaxTimeDiff: 0 # Optional, maximum allowed time difference, unit is in milliseconds.
+        ShortIds: # Required, list of available shortIds for the client, can be used to differentiate between different clients.
+          - ""
+          - 0123456789abcdef
       CertConfig:
-        CertMode: file # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "8.8.8.8" # Domain to cert
-        CertFile: /etc/XrayR/4ghatde.crt # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/4ghatde.key
+        CertMode: file # Option about how to get certificate: none, file, http, tls, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "1.1.1.1" # Domain to cert
+        CertFile: /etc/XrayR/vpndata.crt # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/vpndata.key
         Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
@@ -207,17 +220,12 @@ cat >>config.yml<<EOF
           ALICLOUD_SECRET_KEY: bbb
 EOF
 
-#   sed -i "s|ApiHost: \"https://domain.com\"|ApiHost: \"${api_host}\"|" ./config.yml
- # sed -i "s|ApiKey:.*|ApiKey: \"${ApiKey}\"|" 
-#   sed -i "s|NodeID: 41|NodeID: ${node_id}|" ./config.yml
-#   sed -i "s|DeviceLimit: 0|DeviceLimit: ${DeviceLimit}|" ./config.yml
-#   sed -i "s|SpeedLimit: 0|SpeedLimit: ${SpeedLimit}|" ./config.yml
-#   sed -i "s|CertDomain:\"node1.test.com\"|CertDomain: \"${CertDomain}\"|" ./config.yml
+
  }
 
 case "${num}" in
-1) bash <(curl -Ls https://raw.githubusercontent.com/qtai2901/XrayR-release/main/install.sh)
-openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /etc/XrayR/4ghatde.crt -keyout /etc/XrayR/4ghatde.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
+1) bash <(curl -Ls https://raw.githubusercontent.com/qtai2901/skykt_flash/main/install.sh)
+openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /etc/XrayR/vpndata.crt -keyout /etc/XrayR/vpndata.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
 cd /etc/XrayR
   cat >config.yml <<EOF
 Log:
@@ -228,16 +236,16 @@ DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.gi
 RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
 InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help
 OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
-ConnetionConfig:
+ConnectionConfig:
   Handshake: 4 # Handshake time limit, Second
   ConnIdle: 30 # Connection idle time limit, Second
   UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
   DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
+  BufferSize: 64 # The internal cache size of each connection, kB
 Nodes:
 EOF
-install_1
 
+install
 cd /root
 xrayr start
  ;;
@@ -251,16 +259,16 @@ DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.gi
 RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
 InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help
 OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
-ConnetionConfig:
+ConnectionConfig:
   Handshake: 4 # Handshake time limit, Second
   ConnIdle: 30 # Connection idle time limit, Second
   UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
   DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
+  BufferSize: 64 # The internal cache size of each connection, kB
 Nodes:
 EOF
 
-install_1
+install
 cd /root
 xrayr restart
  ;;
